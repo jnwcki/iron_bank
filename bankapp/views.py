93e8162cf283e83829d60ba542f46e8e9cb91989
jnwcki <jude.nowicki@gmail.com>
2016-03-03 13:50:46 -0500

    initial commit. working homepage

---
+++ bankapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        pass
