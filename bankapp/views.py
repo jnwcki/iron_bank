15eefa5e758548c825317b99330a222163e09cb2
jnwcki <jude.nowicki@gmail.com>
2016-03-05 17:59:52 -0500

    added account balance calculating

--- bankapp/views.py
+++ bankapp/views.py
@@ -49,6 +49,25 @@ class TransactionView(CreateView):
     model = Transaction
     fields = ['amount', 'description', 'account']
 
-
+    #def form_valid(self, form):
+        # need logic to sum up transaction register and update account balance
+        #trans = form.save(commit=False)
+        #trans.save()
+        #form.instance.account = Account.objects.get(self.kwargs['pk'])
+        #pass
     def get_success_url(self):
-        return reverse('index')
+        #print(self.kwargs)
+        account_var = Account.objects.get(pk=self.kwargs['pk'])
+        #my_cust_variable = Customer.objects.get(user=self.request.user)
+        #my_trans_variable = Transaction.objects.filter(account__acctxref__customer=my_cust_variable)
+        my_trans_variable = Transaction.objects.filter(account=account_var)
+        add_money = 0
+
+        for item in my_trans_variable:
+            add_money += item.amount
+        account_var.current_balance = add_money + account_var.beginning_balance
+        account_var.save()
+        #print(add_money)
+        #print(account_var.current_balance)
+        #print(my_trans_variable.values())
+        return reverse('user_profile')
